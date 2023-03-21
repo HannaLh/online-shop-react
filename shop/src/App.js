d28bcb9bb82b6400b9b84b021c80c88855ae92e9
Anna Leoshko <anna.leoshko@gmail.com>
2023-03-21 19:45:15 +0100

    implemented header main and footer

--- shop/src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <div>Shop</div>
-  );
-}
-
-export default App;
---
+++ shop/src/Components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className='footer-navbar'>
+                <nav>
+                    <ul>
+                        <li><a href="/#">Living Room</a></li>
+                        <li><a href="/#">Bedroom</a></li>
+                        <li><a href="/#">Kitchen & Dining</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className='footer-networks-logo'>
+
+            </div>
+            <div className='footer-copyright'>
+                <p>© 2010 — 2020</p>
+                <p>Privacy — Terms</p>
+            </div>
+        </footer>
+
+    )
+}
