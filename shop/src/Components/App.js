ce89489542ae17bce7312fd59242f9107a630681
Anna Leoshko <anna.leoshko@gmail.com>
2023-03-25 19:01:39 +0100

    refactored code

--- shop/src/Components/App.js
+++ shop/src/Components/App.js
@@ -1,15 +1,17 @@
 import Header from "./Header/Header";
 import Main from "./Main/Main";
+import Drawer from "./Drawer/Drawer";
 import Footer from "./Footer/Footer";
 
 function App() {
-  return (
-    <div className='wrapper'>
-        <Header/>
-        <Main/>
-        <Footer/>
-    </div>
-  );
+    return (
+        <div className='wrapper'>
+            <Header/>
+            <Main/>
+            <Drawer/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ shop/src/Components/Drawer/Drawer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Drawer.css";
+
+export default function Drawer() {
+    return(
+        <div className='overlay'>
+            <div className='drawer'>
+                <h3>Cart</h3>
+                <div className='cart-item'>
+                    <img src="" alt="cart-item"></img>
+                    <p></p>
+                </div>
+                <button></button>
+            </div>
+        </div>
+    )
+}
