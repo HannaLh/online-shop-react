a499e0432f2a56fa28283f141337c47ecc22b3cc
Andrii Yudov <andrii.yudov@pitchbook.com>
2023-06-08 23:19:12 +0300

    Delete hooks file

--- shop/src/store/hooks.ts
+++
@@ -1,6 +0,0 @@
-import {useDispatch, useSelector, TypedUseSelectorHook} from 'react-redux';
-
-import type {RootState, Dispatch} from './index';
-
-export const useAppDispatch = () => useDispatch<Dispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
