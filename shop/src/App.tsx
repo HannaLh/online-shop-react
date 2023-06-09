713525a10b7ca652406da3405d4084b8c893cc3e
Andrii Yudov <andrii.yudov@pitchbook.com>
2023-06-09 22:12:42 +0300

    Edit import path to components

--- shop/src/App.tsx
+++ shop/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 
-import Home from './pages/home';
-import Cart from './pages/cart';
-import NotFoundBlock from './pages/not-found-block';
-import {MainLayout} from './components/main-layout/main-layout';
+import Home from 'pages/home';
+import Cart from 'pages/cart';
+import NotFoundBlock from 'pages/not-found-block';
+import {MainLayout} from 'components/main-layout/main-layout';
 
 import './App.scss';
 
--- shop/src/pages/home/home.tsx
+++ shop/src/pages/home/home.tsx
@@ -4,12 +4,12 @@ import qs from 'qs';
 import {useNavigate} from 'react-router-dom';
 
 import {filterSelector, setCategoryId, setCurrentPage} from 'store/reducers/filter/filter';
-import {Categories} from '../../components/categories/categories';
-import {Sort} from '../../components/sort/sort';
-import {FurnitureCard} from '../../components/furniture-card/furniture-card';
-import {Skeleton} from '../../components/furniture-card/furniture-card-skeleton';
-import {MainBanner} from '../../components/main-banner/main-banner';
-import {Pagination} from '../../components/pagination/pagination';
+import {Categories} from 'components/categories/categories';
+import {Sort} from 'components/sort/sort';
+import {FurnitureCard} from 'components/furniture-card/furniture-card';
+import {Skeleton} from 'components/furniture-card/furniture-card-skeleton';
+import {MainBanner} from 'components/main-banner/main-banner';
+import {Pagination} from 'components/pagination/pagination';
 import {fetchFurniture, furnitureDataSelector} from 'store/reducers/furniture/furniture';
 
 import './home.scss';
