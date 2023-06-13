5049392051bf9ea70bd97137064f6ffd81af6eb5
Anna  Leoshko <anna.leoshko@gmail.com>
2023-06-13 11:06:05 +0200

    changed file name from index to Pagination.tsx

---
+++ shop/src/Components/Pagination/Pagination.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+
+import './Pagination.scss';
+
+type PaginationProps = {
+    currentPage: number;
+    onChangePage: (page: number) => void;
+};
+
+export const Pagination: React.FC<PaginationProps> = ({currentPage, onChangePage}) => (
+    <ReactPaginate
+        className="pagination"
+        breakLabel="..."
+        nextLabel=">"
+        previousLabel="<"
+        onPageChange={(event) => onChangePage(event.selected + 1)}
+        pageRangeDisplayed={4}
+        pageCount={3}
+        forcePage={currentPage - 1}
+        renderOnZeroPageCount={null}
+    />
+);
