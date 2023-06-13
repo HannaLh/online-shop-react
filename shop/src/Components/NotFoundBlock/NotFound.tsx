4f1589ad5fb4508900c9ba996eccd3b9407580d9
Anna  Leoshko <anna.leoshko@gmail.com>
2023-06-13 11:00:31 +0200

    changed file name from index to NotFound

---
+++ shop/src/Components/NotFoundBlock/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './NotFoundBlock.module.css';
+
+export const NotFoundBlock: React.FC = () => {
+    return (
+        <h1 className={styles.root}>
+            <span>😔</span>
+            <br />
+            Oops, nothing was found!
+        </h1>
+    );
+};
