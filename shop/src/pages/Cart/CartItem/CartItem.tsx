b8d916a0b1023150802d5d30e7779acebc30058b
Anna  Leoshko <anna.leoshko@gmail.com>
2023-06-14 12:17:25 +0200

    refactored styles in Cart

--- shop/src/pages/Cart/CartItem/CartItem.tsx
+++ shop/src/pages/Cart/CartItem/CartItem.tsx
@@ -39,17 +39,19 @@ export const CartItem: React.FC<CartItemProps> = ({id, title, price, count, imag
             <div className="cart-item__image-box">
                 <img className='cart-item__image' src={imageUrl} alt="cart item" width="190" height="200"></img>
             </div>
-            <div className="cart-item__about">
-                <h1 className="cart-item__title">{title}</h1>
-            </div>
-            <div className="cart-item__counter">
-                <button onClick={onClickPlus} className="cart-item__btn">+</button>
-                <div className="cart-item__count">{count}</div>
-                <button disabled={count === 1} onClick={onClickMinus} className="cart-item__btn">-</button>
-            </div>
-            <div className="cart-item__prices">
-                <div className="cart-item__amount">${price * count}</div>
-                <button onClick={onClickRemove} className="cart-item__remove"><u>Remove</u></button>
+            <div className="cart-item__product-column">
+                <div className="cart-item__about">
+                    <h1 className="cart-item__title">{title}</h1>
+                </div>
+                <div className="cart-item__counter">
+                    <button onClick={onClickPlus} className="cart-item__btn">+</button>
+                    <div className="cart-item__count">{count}</div>
+                    <button disabled={count === 1} onClick={onClickMinus} className="cart-item__btn">-</button>
+                </div>
+                <div className="cart-item__prices">
+                    <div className="cart-item__amount">${price * count}</div>
+                    <button onClick={onClickRemove} className="cart-item__remove"><u>Remove</u></button>
+                </div>
             </div>
         </div>
     );
